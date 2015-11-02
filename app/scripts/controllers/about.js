9167150249019efd0aae9af72c15beddd0a33fd5
Eduardo Cespedes <lalocespedes@gmail.com>
2015-11-01 20:03:43 -0600

    yo angular install

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularslimApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularslimApp
+ */
+angular.module('angularslimApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularslimApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularslimApp
+ */
+angular.module('angularslimApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
